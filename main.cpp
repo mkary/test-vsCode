f4283a0769b24cd71077a7746e85dd2549b1bc1a
Teszt Elek <my@teszt.com>
2017-09-07 19:03:37 +0200

    teszt2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include <vector>
 
 int main()
 {
-    std::cout << "Hello World!";
-    auto e = 10;
+    std::vector<int> list {0,1,2,3,4,5,6,7,8,9};
+
+    for(int value: list)
+    {
+        std::cout << value << " ";
+    }
     return 0;
 }
